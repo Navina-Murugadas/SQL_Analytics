c5d0db5c74196294f2604a75677f14689f130207
Navina M <navina.mk7@gmail.com>
2026-02-18 06:08:16 +0530

    Update Day 2 — Employees Earning More Than Their Managers.sql

--- daily_sql_learning/02-joins-and-self-joins/Day 2 — Employees Earning More Than Their Managers.sql
+++ daily_sql_learning/02-joins-and-self-joins/Day 2 — Employees Earning More Than Their Managers.sql
@@ -1,3 +1,6 @@
+---------------------------------------------------------------------------------------------
+-- GOAL: Find employees whose salary is higher than their manager's.
+---------------------------------------------------------------------------------------------
 create table emp(emp_id int,emp_name varchar(10),salary int ,manager_id int);
 
 insert into emp values(1,'Ankit',10000,4);
@@ -8,10 +11,10 @@ insert into emp values(5,'Mudit',12000,6);
 insert into emp values(6,'Agam',12000,2);
 insert into emp values(7,'Sanjay',9000,2);
 insert into emp values(8,'Ashish',5000,2);
-
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
 select * from emp;
 
--- Goal: Find employees whose salary is higher than their manager's.
 SELECT e.emp_id, 
 	   e.emp_name, 
 	   m.emp_name AS manager_name, 
@@ -21,5 +24,4 @@ FROM emp e
 INNER JOIN emp m
 ON e.manager_id = m.emp_id
 WHERE e.salary > m.salary;
-
 
