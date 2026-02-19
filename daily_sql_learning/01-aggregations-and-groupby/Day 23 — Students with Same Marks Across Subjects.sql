ac28eccdb633e1be7e015bd2b47113b017a0c811
Navina M <navina.mk7@gmail.com>
2026-02-19 05:40:36 +0530

    Add files via upload

---
+++ daily_sql_learning/01-aggregations-and-groupby/Day 23 — Students with Same Marks Across Subjects.sql
@@ -0,0 +1,19 @@
+---------------------------------------------------------------------------------------------
+-- GOAL: Find students who scored the same marks in Chemistry and Physics.
+---------------------------------------------------------------------------------------------
+create table exams (student_id int, subject varchar(20), marks int);
+delete from exams;
+insert into exams values (1,'Chemistry',91),(1,'Physics',91)
+,(2,'Chemistry',80),(2,'Physics',90)
+,(3,'Chemistry',80)
+,(4,'Chemistry',71),(4,'Physics',54);
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+SELECT * FROM exams;
+
+SELECT student_id
+FROM exams
+WHERE subject IN ('Chemistry', 'Physics')
+GROUP BY student_id
+HAVING COUNT(DISTINCT subject) = 2
+   AND COUNT(DISTINCT marks) = 1;
