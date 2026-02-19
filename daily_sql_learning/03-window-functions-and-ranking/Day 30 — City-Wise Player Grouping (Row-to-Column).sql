a8fd0ab25b1333d88b78519d88bcbb6bc06029a9
Navina M <navina.mk7@gmail.com>
2026-02-19 06:03:00 +0530

    Add files via upload

---
+++ daily_sql_learning/03-window-functions-and-ranking/Day 30 — City-Wise Player Grouping (Row-to-Column).sql
@@ -0,0 +1,39 @@
+--------------------------------------------------------------------------------------------------------------
+-- GOAL: Display each city as a column, with players sorted alphabetically and aligned by group number.
+--------------------------------------------------------------------------------------------------------------
+/*
+Display each city as a separate column
+Within each city, players must be sorted alphabetically
+The alphabetically 1st player from every city should belong to Group 1
+The alphabetically 2nd player from every city should belong to Group 2
+Continue grouping in this manner for all remaining players
+If a city has fewer players, leave the cell blank (NULL)
+*/
+--------------------------------------------------------------------------------------------------------------
+create table players_location
+(
+name varchar(20),
+city varchar(20)
+);
+
+insert into players_location
+values ('Sachin','Mumbai'),('Virat','Delhi'),('Rahul','Bangalore'),('Rohit','Mumbai'),('Mayank','Bangalore');
+--------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------
+SELECT * FROM players_location;
+
+WITH Group_number AS (
+	SELECT *,
+		   ROW_NUMBER() OVER(PARTITION BY city ORDER BY name ASC)
+				AS grp_no
+	FROM players_location)
+SELECT 
+	MAX(CASE WHEN city = 'Bangalore' THEN name 
+		END) AS Bangalore,
+	MAX(CASE WHEN city = 'Delhi' THEN name
+		END) AS Delhi,
+	MAX(CASE WHEN city = 'Mumbai' THEN name
+		END) AS Mumbai
+FROM Group_number
+GROUP BY grp_no
+ORDER BY grp_no;
