8701ba4f0f89370b08c702b9d217bced91a081c0
Navina M <navina.mk7@gmail.com>
2026-02-19 05:45:28 +0530

    Add files via upload

---
+++ daily_sql_learning/05-date-and-time-analytics/Day 22 — Detecting Missing Quarters in Time Series.sql
@@ -0,0 +1,59 @@
+---------------------------------------------------------------------------------------------
+-- GOAL: Find which Q1–Q4 is missing for each store.
+---------------------------------------------------------------------------------------------
+CREATE TABLE STORES (
+Store varchar(10),
+Quarter varchar(10),
+Amount int);
+
+INSERT INTO STORES (Store, Quarter, Amount)
+VALUES ('S1', 'Q1', 200),
+('S1', 'Q2', 300),
+('S1', 'Q4', 400),
+('S2', 'Q1', 500),
+('S2', 'Q3', 600),
+('S2', 'Q4', 700),
+('S3', 'Q1', 800),
+('S3', 'Q2', 750),
+('S3', 'Q3', 900);
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+SELECT * FROM STORES;
+
+-- Using CAST & GROUP BY:
+SELECT Store,
+	   'Q'+ CAST(10 - SUM(CAST(RIGHT(Quarter, 1) AS INT)) AS CHAR(2)) AS Q_no
+FROM STORES
+GROUP BY Store;
+
+---------------------------------------------------------------------------------------------
+-- Using RECURSIVE CTE:
+WITH quar AS (
+	SELECT DISTINCT Store, 1 AS Q_no
+	FROM STORES
+	UNION ALL
+	SELECT Store, Q_no + 1 AS Q_no
+	FROM quar
+	WHERE Q_no < 4),
+quarts AS (
+	SELECT Store, 'Q' + CAST(Q_no AS CHAR(1)) AS Q_no
+	FROM quar)
+SELECT qu.* 
+FROM quarts qu
+LEFT JOIN STORES S
+	ON qu.Store = S.Store
+	AND qu.Q_no = S.Quarter
+WHERE S.Store IS NULL
+ORDER BY Store;
+
+---------------------------------------------------------------------------------------------
+-- Using CROSS JOIN:
+WITH quarters AS (
+	SELECT DISTINCT s1.Store, s2.Quarter AS Q_no
+	FROM STORES s1, STORES s2)
+SELECT q.Store, q.Q_no
+FROM quarters q
+LEFT JOIN STORES s
+	ON q.Store = s.Store
+	AND q.Q_no = s.Quarter
+WHERE s.Quarter IS NULL;
---
+++ daily_sql_learning/05-date-and-time-analytics/Day 29 — Login and Logout Interval Analysis.sql
@@ -0,0 +1,32 @@
+---------------------------------------------------------------------------------------------
+-- GOAL: Identify login–logout intervals and calculate active minutes from ON/OFF event logs.
+---------------------------------------------------------------------------------------------
+create table event_status
+(
+event_time varchar(10),
+status varchar(10)
+);
+
+insert into event_status 
+values ('10:01','on'),('10:02','on'),('10:03','on'),('10:04','off'),
+('10:07','on'),('10:08','on'),('10:09','off'),('10:11','on'),('10:12','off');
+---------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------
+SELECT * FROM event_status;
+
+WITH Previous_status AS (
+	SELECT *,
+		   LAG(status,1,status) OVER(ORDER BY event_time ASC) 
+			 AS prev_status
+	FROM event_status),
+Group_key AS (
+	SELECT *,
+		   SUM(CASE WHEN status = 'on' AND prev_status = 'off' 
+				THEN 1 ELSE 0 END) 
+			  OVER(ORDER BY event_time ASC) AS grp_status
+	FROM Previous_status)
+SELECT MIN(event_time) AS Login_time,
+	   MAX(event_time) AS Logout_time,
+	   COUNT(*)-1 AS Active_mins
+FROM Group_key
+GROUP BY grp_status;
